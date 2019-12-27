ffbfa840e1226737c090d6c391175009360ff2a6
Suhan Moon <lunyx@me.com>
2019-12-27 17:51:38 +0900

    a

--- components/Button/index.native.js
+++ components/Button/index.native.js
@@ -1,61 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
-import _styles from './style';
+import React from 'react';
 
-const styles = StyleSheet.create(_styles);
-// {
-//   default: {
-//     height: 40,
-//     backgroundColor: '#1890ff',
-//     borderColor: '#1890ff',
-//     borderWidth: 1,
-//     borderRadius: 4,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   defaultText: {
-//     color: '#FFF',
-//     fontSize: 14,
-//     fontWeight: 'bold',
-//   },
-// });
-
-const coverStyle = [styles['ab-button']];
-
-const Button = ({ children }) => {
-
-  console.log(styles);
-
-  try {
-
-    const [pressed, setPressed] = useState(false);
-    useEffect(() => () => {
-      if (pressed) {
-        coverStyle.push({
-          backgroundColor: "#F00"
-        })
-      }
-      else {
-        coverStyle.splice(1);
-      }
-    }, [pressed]);
-
-
-
-    return (
-      <TouchableOpacity
-        activeOpacity={1}
-        onPressIn={() => setPressed(true)}
-        onPressOut={() => setPressed(false)}
-        style={coverStyle}
-      >
-        <Text style={[styles.defaultText]}>{children}</Text>
-      </TouchableOpacity>
-    );
-  }
-  catch(e) {
-
-  }
+const Button = (props) => {
+  return (
+    <button>
+    </button>
+  );
 };
 
 export default Button;
