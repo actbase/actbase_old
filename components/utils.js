521aa763f3bceee1b6c8baaca66dcadac06b938d
Suhan Moon <lunyx@me.com>
2019-12-28 18:35:07 +0900

    button with css

---
+++ components/utils.js
@@ -0,0 +1,25 @@
+export const TEXT_STYLE_NAMES = [
+  'color',
+  'fontFamily',
+  'fontSize',
+  'fontStyle',
+  'fontWeight',
+  'fontVariant',
+  'textShadowOffset',
+  'textShadowRadius',
+  'textShadowColor',
+  'letterSpacing',
+  'lineHeight',
+  'textAlign',
+  'textAlignVertical',
+  'includeFontPadding',
+  'textDecorationLine',
+  'textDecorationStyle',
+  'textDecorationColor',
+  'textTransform',
+  'writingDirection',
+];
+
+export default {
+  TEXT_STYLE_NAMES,
+};
