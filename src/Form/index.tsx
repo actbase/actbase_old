f973945c54a89d6eea2692c6e0f21d7988a7fd19
Suhan Moon <leader@trabricks.io>
2020-02-15 19:20:13 +0900

    최종 스트럭쳐 잡고 설정

--- src/Form/index.tsx
+++ src/Form/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useState } from 'react';
+import * as React from 'react';
 import { forIn, isEqual } from 'lodash';
 import View from '../web/View';
 import { measure } from '../App/utils';
@@ -10,30 +10,52 @@ export interface FormProps {
   onLayout: any;
 }
 
+export interface ResultProps {
+  returnKeyType?: string;
+  onSubmitEditing?: any;
+}
+
+export interface ResultData {
+  [key: string]: any;
+}
+
+export interface FieldItems {
+  [key: string]: {
+    input: any;
+    options: {
+      name?: string;
+      focus?: boolean;
+      setProps: any;
+      getValue: any;
+    };
+    area: number;
+  };
+}
+
 export const FormContext = React.createContext({});
 
 const Form: React.FC<FormProps> = (props: FormProps): React.ReactElement => {
-  const items = useRef<any>({});
-  const index = useRef<number>(0);
-  const subscribe = useCallback((oRef, input, options) => {
+  const items = React.useRef<FieldItems>({});
+  const index = React.useRef<number>(0);
+  const subscribe = React.useCallback((oRef, input, options) => {
     let name = oRef.current;
     if (!name) {
       name = `FormField_${index.current}`;
       oRef.current = name;
       index.current = index.current + 1;
     }
-    items.current[name] = { input, options };
+    items.current[name] = { input, options, area: 0 };
   }, []);
 
-  const unsubscribe = useCallback(oRef => {
+  const unsubscribe = React.useCallback(oRef => {
     let name = oRef.current;
     delete items.current[name];
   }, []);
 
-  const [lastLayout, setLastLayout] = useState({});
+  const [lastLayout, setLastLayout] = React.useState({});
   const { onLayout, ...oProps } = props;
 
-  const handleLayout = useCallback(async e => {
+  const handleLayout = React.useCallback(async e => {
     onLayout && onLayout(e);
     const { width, height } = e.nativeEvent.layout;
     const pos = { width, height };
@@ -44,53 +66,50 @@ const Form: React.FC<FormProps> = (props: FormProps): React.ReactElement => {
       const el = items?.current[key];
       if (el.input) {
         const pos = await measure(el.input);
-        const area = parseFloat(`${Math.floor(pos?.pageY)}.${Math.floor(pos?.pageX)}`);
+        const area = -parseFloat(`${Math.floor(pos?.pageY)}.${Math.floor(pos?.pageX)}`);
         items.current[key].area = area;
       }
     }
 
     const elements = Object.values(items.current)
-        // @ts-ignore
       ?.filter(v => !!v.options.focus)
-      ?.sort((a: any, b: any) => (a.area > b.area ? 1 : a.area < b.area ? -1 : 0));
+      ?.sort((a, b) => (a.area < b.area ? 1 : a.area > b.area ? -1 : 0));
 
     elements?.forEach((v: any, index: number) => {
-      const args: any = {};
+      const args: ResultProps = {};
       if (elements.length - 1 <= index) {
         args.returnKeyType = 'done';
         args.onSubmitEditing = submit;
       } else {
         args.returnKeyType = 'next';
-        // @ts-ignore
         args.onSubmitEditing = elements[index + 1].options.focus;
       }
       v.options?.setProps?.(args);
     });
   }, []);
 
-  const submit = useCallback(async () => {
+  const submit = React.useCallback(async () => {
     const elements = Object.values(items?.current).filter(v => {
-      // @ts-ignore
       v.options?.setProps?.({ submitting: true });
-      // @ts-ignore
       return !!v.options?.name;
     });
 
-    const params: any = {};
+    const params: ResultData = {};
     elements.forEach(v => {
-      // @ts-ignore
       const opt = v.options;
-      if (params[opt.name] === undefined) {
-        params[opt.name] = opt.getValue();
-      } else if (params[opt.name]?.push !== undefined) {
-        params[opt.name].push(opt.getValue());
-      } else if (params[opt.name] !== null && result[opt.name] !== undefined) {
-        const values = [...params[opt.name], opt.getValue()];
-        params[opt.name] = values;
+      if (opt?.name) {
+        if (params[opt.name] === undefined) {
+          params[opt.name] = opt.getValue();
+        } else if (params[opt.name]?.push !== undefined) {
+          params[opt.name].push(opt.getValue());
+        } else if (params[opt.name] !== null && params[opt.name] !== undefined) {
+          const values = [...params[opt.name], opt.getValue()];
+          params[opt.name] = values;
+        }
       }
     });
 
-    let result = params;
+    let result: ResultData | FormData = params;
     if (props.output === 'FormData') {
       result = new FormData();
       forIn(params, (value, name) => {
